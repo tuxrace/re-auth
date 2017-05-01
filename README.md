1f2cc2c74238f846307bfea99abf65012a0fd946
Arman Ortega <sudogem@gmail.com>
2017-05-01 22:13:40 +0800

    Added react-bootstrap library

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 
 #### Installation
 $ npm install flow-bin -g  
+$ npm install yarn -g  
 $ yarn install  
 $ yarn flow    # (manually run this for type-checking of your code)  
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,12 +2,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { hello } from '../actions/index'
+import { Button } from 'react-bootstrap'
 
 const Timeline = ({store, hello}: { store: Object, hello: Function }) => (
   <div>
     <h1>Timeline</h1>
     {store.persons.forEach(x => {
-      return <div> {x.user} </div>
+      return (
+        <div>
+          <div> {x.user} </div>
+          <Button bsStyle="success" bsSize="small" onClick={test}>Nope</Button>
+          <Button bsStyle="success" bsSize="small" onClick={test}>Like</Button>
+        </div>
+      )
     })}
     <button onClick={() => hello('Hi arman')}> Test </button>
     {store.greeting}
